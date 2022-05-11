b0b9ed9e2a09a1753adf4ce0b350e4c1dd563be9
Yuki Ota <yukiotataitien@gmail.com>
2022-05-11 23:39:15 +0800

    feat(redis): Remove redis

--- index.js
+++ index.js
@@ -6,17 +6,7 @@ const app = express();
 app.use(express.static(process.cwd()));
 import cors from 'cors';
 import * as http from 'http';
-import redis from 'redis';
-const client = redis.createClient({
-  port      : 6379,
-  host      : process.env.REDIS_HOST
-});
 const server = http.createServer(app);
-
-// connect redis server
-client.on("error", function(error) {
-  console.error(error);
-});
 
 /* libs */
 import { events } from './lib/events.js';
@@ -76,46 +66,21 @@ app.get('/api/district', cors(corsOptions),(request, response) => {
 app.get('/api/events', cors(corsOptions),async(request, response) => {
   try {
 
-    client.get("events", async (err, redisData) => {
-      // err handle
-      if (err) {
-        console.log(err)
-      }
-      // check redis has value
-      if (redisData) {
-        response.json({
-          status: true,
-          message: '資料取得成功',
-          data: JSON.parse(redisData),
-        });
-      } else {
-        const data = await events;
+    const data = await events;
 
-        if(data) {
-          if (data.length > 0) {
-            client.set("events", JSON.stringify(data), redis.print);
-            client.expire("events", 12*60*60);
-            response.json({
-              status: true,
-              message: '資料取得成功',
-              data,
-            });
-          } else {
-            response.status(404).send({
-              status: false,
-              message: '無法取得更新資料',
-              data: null,
-            });
-          }
-        } else {
-          response.status(404).send({
-            status: false,
-            message: '查無資料',
-            data: null,
-          });
-        }
-      }
-    });
+    if(data && data.length > 0) {
+      response.json({
+        status: true,
+        message: '資料取得成功',
+        data,
+      });
+    } else {
+      response.status(404).send({
+        status: false,
+        message: '查無資料',
+        data: null,
+      });
+    }
 
   } catch (err) {
     console.log(err);
