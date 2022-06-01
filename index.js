5a42edc104d98c9da1e3c023edf0d1e490917242
BiBiOTA <yukiotataitien@gmail.com>
2022-06-01 23:10:51 +0800

    feat(log): console.log write to log file

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ const server = http.createServer(app);
 import bodyParser from 'body-parser';
 const jsonParser = bodyParser.json();
 
+/* log */
+import fs from 'fs';
+import util from 'util';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
+var log_stdout = process.stdout;
+console.log = function(d) { //
+  log_file.write(util.format(d) + '\n');
+  log_stdout.write(util.format(d) + '\n');
+};
+
 /* libs */
 import { events } from './lib/events.js';
 import { district } from './lib/district.js';
