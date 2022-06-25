af3b16528a84b6195be8b2b8576476ffe258fffc
Yuki Ota <yukiotataitien@gmail.com>
2022-06-25 20:17:27 +0800

    feat(env): Add env variable

--- setup_env.sh
+++ setup_env.sh
@@ -5,3 +5,4 @@ echo APP_NAME=irunning-node >> .env
 echo TELEGRAM_TOKEN=$TELEGRAM_TOKEN >> .env
 echo LARAVEL_API_URL=$LARAVEL_API_URL >> .env
 echo NODE=production >> .env
+echo MY_SLACK_WEBHOOK_URL=$MY_SLACK_WEBHOOK_URL >> .env
